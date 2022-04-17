93760e58dee2ff677cd56b72c23b6f5a036343ee
ViktorPires <viktor_max@icloud.com>
2022-04-17 12:35:11 -0300

    API v1.0

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemModule } from './item/item.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      logger: 'advanced-console',
+      logging: true,
+    }),
+    ItemModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/item/item.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MoreThanOrEqual, Repository } from 'typeorm';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+import { Item } from './entities/item.entity';
+
+@Injectable()
+export class ItemService {
+  constructor(
+    @InjectRepository(Item) private readonly repository: Repository<Item>,
+  ) {}
+
+  create(createItemDto: CreateItemDto): Promise<Item> {
+    const item = this.repository.create(createItemDto);
+    return this.repository.save(item);
+  }
+
+  findAll(): Promise<Item[]> {
+    return this.repository.find({
+      order: { name: 'ASC' },
+      where: { quantity: MoreThanOrEqual(0) },
+    });
+  }
+
+  async findOne(id: string): Promise<Item> {
+    const item = await this.repository.findOne(id);
+
+    if (!item) {
+      throw new NotFoundException(`Item ${id} not found`);
+    }
+    return;
+  }
+  async update(id: string, updateItemDto: UpdateItemDto): Promise<Item> {
+    const item = await this.repository.preload({
+      id: id,
+      ...updateItemDto,
+    });
+    if (!item) {
+      throw new NotFoundException(`Item ${id} not found`);
+    }
+    return this.repository.save(item);
+  }
+
+  async remove(id: string) {
+    const item = await this.findOne(id);
+    return this.repository.remove(item);
+  }
+}
